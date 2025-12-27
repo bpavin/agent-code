a521f534ceaa2253c61eb59eb434a5200ee54e0f
Borna P <bpavin1@gmail.com>
2025-12-27 11:28:59 +0100

    Implemented tool calls.

--- llm-code-cli.asd
+++ llm-code-cli.asd
@@ -1,20 +1,20 @@
-(defsystem "llm-local"
+(defsystem "llm-code-cli"
   :version "0.0.1"
   :author ""
   :license ""
   :class :package-inferred-system
   :defsystem-depends-on (:asdf-package-system)
-  :depends-on ("llm-local/src/main")
+  :depends-on ("llm-code-cli/src/main")
   :description ""
-  :in-order-to ((test-op (test-op "llm-local/tests"))))
+  :in-order-to ((test-op (test-op "llm-code-cli/tests"))))
 
-(defsystem "llm-local/tests"
+(defsystem "llm-code-cli/tests"
   :author ""
   :license ""
-  :depends-on ("llm-local"
+  :depends-on ("llm-code-cli"
                "rove")
   :components ((:module "tests"
                 :components
                 ((:file "main"))))
-  :description "Test system for llm-local"
+  :description "Test system for llm-code-cli"
   :perform (test-op (op c) (symbol-call :rove :run c)))
---
+++ src/tool.lisp
@@ -0,0 +1,80 @@
+(defpackage :llm-code-cli/src/tool
+	(:use :cl)
+    (:nicknames :tool)
+	(:export
+     #:tool
+
+     #:name
+     #:description
+     #:parameters
+
+     #:to-alist
+     #:tool-execute
+
+     #:read-tool
+     #:write-tool
+     #:delete-tool))
+
+(in-package :llm-code-cli/src/tool)
+
+(defclass tool ()
+    ((name :type string :accessor name)
+     (description :type string :accessor description)
+     (parameters :type list :accessor parameters)
+     (required :type list :accessor required)))
+
+(defgeneric tool-execute (this args)
+  (:documentation "Abstract method for tool implementation."))
+
+(defgeneric to-alist (this)
+  (:documentation "Abstract method that returns alist description of the tool."))
+
+(defmethod to-alist (this)
+    (break)
+    `((:name . ,(name this))
+      (:description . ,(description this))
+      (:parameters .
+            ((:type . :object)
+             (:properties . ,(parameters this))))
+      (:required . ,(required this))))
+
+(defun aget (alist item)
+  (alexandria:assoc-value alist item))
+
+(defclass read-tool (tool)
+  ((name :initform "read_file")
+   (description :initform "Reads a file from disk.")
+   (parameters :initform '((:path . ((:type . :string)
+                                     (:description . "Absolute path of the file.")))))
+   (required :initform '(:path))))
+
+(defmethod tool-execute ((tool read-tool) args)
+  (if (null args)
+      (error "No file specified for reading.")
+      (alexandria:read-file-into-string (aget args :path))))
+
+(defclass write-tool (tool)
+  ((name :initform "write_file")
+   (description :initform "Writes content to a file on disk.")
+   (parameters :initform '((:path . ((:type . :string)
+                                     (:description . "Absolute path of the file.")))
+                           (:content . ((:type . :string)
+                                        (:description . "Content of the file.")))))
+   (required :initform '(:path :content))))
+
+(defmethod tool-execute ((tool write-tool) args)
+  (if (< (length args) 2)
+      (error "Not enough arguments specified for writing.")
+      (alexandria:write-string-into-file (aget args :content) (aget args :path))))
+
+(defclass delete-tool (tool)
+  ((name :initform "delete_file")
+   (description :initform "Deletes a file from disk.")
+   (parameters :initform '((:path . ((:type . :string)
+                                     (:description . "Absolute path of the file.")))))
+   (required :initform '(:path))))
+
+(defmethod tool-execute ((tool delete-tool) args)
+  (if (null args)
+      (error "No file specified for deletion.")
+      (delete-file (aget args :path))))
