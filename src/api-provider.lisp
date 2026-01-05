b311655459dcdfd286cc99ea97df908e5b389bed
Borna P <bpavin1@gmail.com>
2026-01-05 08:32:08 +0100

    Change temperature to 0.6

--- src/api-provider.lisp
+++ src/api-provider.lisp
@@ -39,7 +39,7 @@
                 ~A
                 \"summary\": \"concise\",
                 \"stream\": false,
-                \"temperature\": 0.8,
+                \"temperature\": 0.6,
                 \"max_tokens\": 20000
             }"
           (cl-json:encode-json-to-string model)
@@ -133,7 +133,7 @@
                 ~A
                 \"summary\": \"concise\",
                 \"stream\": false,
-                \"temperature\": 0.8,
+                \"temperature\": 0.6,
                 \"max_output_tokens\": 20000
           }"
           (cl-json:encode-json-to-string model)
