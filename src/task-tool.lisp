e6b9d5290904b25a07b5cb71362c3c43232c9cb7
Borna P <bpavin1@gmail.com>
2026-04-05 12:36:17 +0200

    task-tool: improvements in listing tasks, updating and removing starts from index 1

--- src/task-tool.lisp
+++ src/task-tool.lisp
@@ -19,10 +19,12 @@
 Insert new task item or update/delete existing. List will return all the tasks currently in the list.")))
                                 (:index . ((:type . :integer)
                                            (:description . "Tasks are kept in a list. Each task item is prefixed with index.
-First index is zero.
+First index is one.
 Use this index to specify which task item you want to update. Index is mandatory for update and delete.")))
                                 (:content . ((:type . :string)
-                                             (:description . "Information you want to update task with. Content is mandatory for insert and update.")))))
+                                             (:description . "Information you want to update task with. Content is mandatory for insert and update.")))
+                                (:status . ((:type . :string)
+                                            (:description . "Status of the task. Can be 'pending', 'approved'. Status is mandatory for insert and update.")))))
    (tool:required :initform '(:operation))
    (history :initform nil :accessor history)))
 
@@ -38,24 +40,55 @@ Use this index to specify which task item you want to update. Index is mandatory
       ("list"
        (if (= 0 (length (history tool)))
            "Task list is empty."
-           (history tool)))
+           (let ((c 0))
+             (reduce (lambda (sum i)
+                       (format nil "~A~%Task id: ~A~%~%Task status: ~A~%~%Task content:~%~A~%-----"
+                               sum
+                               (incf c)
+                               (tool:aget i :status)
+                               (tool:aget i :content)))
+                     (history tool)
+                     :initial-value ""))))
+
       ("insert"
-       (let ((content (tool:aget args :content)))
+       (let ((content (tool:aget args :content))
+             (status (tool:aget args :status)))
          (if (null content)
              (error "Insert requires content."))
+         (if (null status)
+             (error "Insert requires status."))
          (setf (history tool)
                (append (history tool)
-                       (list content))))
+                       (list
+                        (list (cons :content content)
+                              (cons :status status))))))
        "Task successfully inserted.")
+
       ("update"
-       (let ((i (- (parse-integer (tool:aget args :index)) 1))
-             (content (tool:aget args :content)))
-         (setf (nth i (history tool)) content)
+       (let* ((index (if (stringp (tool:aget args :index))
+                         (parse-integer (tool:aget args :index))
+                         (tool:aget args :index)))
+              (i (max 0 (- index 1)))
+              (content (tool:aget args :content))
+              (status (tool:aget args :status)))
+         (if (null content)
+             (error "Insert requires content."))
+         (if (null status)
+             (error "Insert requires status."))
+         (setf (nth i (history tool))
+               (list (cons :content content)
+                     (cons :status status)))
          "Task successfully updated."))
+
       ("remove"
-       (setf (history tool)
-             (delete (nth (tool:aget args :index) (history tool))
-                     (history tool)))
+       (let* ((index (if (stringp (tool:aget args :index))
+                         (parse-integer (tool:aget args :index))
+                         (tool:aget args :index)))
+              (i (max 0 (- index 1))))
+         (setf (history tool)
+               (delete (nth i (history tool))
+                       (history tool))))
        "Task successfully removed.")
+
       (t
        (error "Invalid operation: ~A" operation)))))
